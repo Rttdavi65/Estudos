67350e54b31c84279084afc00e45d96f015896f4
Rttdavi65 <rtttdavi@gmail.com>
2025-03-13 17:22:04 -0300

    atualizando o jokenpo

--- Mundo2/aula1/ex10.py
+++ Mundo2/aula1/ex10.py
@@ -1,26 +1,27 @@
 from random import choice
 from time import sleep
 
-jokenpo = ['Pedra','papel','Tesoura']
-escolha_do_jogador = str(input('Qual Deseja escolher?\n[1] Pedra\n[2] Papel\n[3] Tesoura\n'))
-escolha_do_pc = choice(jokenpo)
-print('Estou escolhendo...')
-sleep(3)
-print('Escolhi! Vamos jogar!')
-sleep(1)
-print('JO')
-sleep(0.5)
-print('KEN')
-sleep(0.5)
-print('PÔ!')
-sleep(1)
-#print(f'Eu joguei {escolha_do_pc}!')
-if escolha_do_jogador == '1':
-    if escolha_do_pc == [0]:
+print('-=-' *10)
+titulo = 'JOKENPÔ!'
+print(f'{titulo :^26}')
+
+def jokenpo():
+    escolha = ['pedra', 'papel', 'tesoura']
+    jogador = str(input('Escolha uma opção:\nPedra\nPapel\nTesoura\n '.strip().title()))
+    
+    if jogador not in escolha:
+        print('Opção invalida!')
+        return
+    
+    pc = choice(escolha)
+    print(f'A Escolha da maquina foi: {pc}')
+
+    if jogador == pc:
         print('Empate!')
-elif escolha_do_jogador == '2':
-    if escolha_do_pc == [1]:
-        print('Empate!')
-elif escolha_do_jogador == '3':
-    if escolha_do_pc == [2]:
-        print('Empate!')
+    elif (jogador == 'pedra' and pc == 'tesoura') or \
+         (jogador == 'papel' and pc == 'pedra') or \
+         (jogador == 'tesoura' and pc == 'papel'):
+        print('Você venceu!')
+    else:
+        print('Você perdeu!')
+jokenpo()
